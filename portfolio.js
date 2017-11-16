c34b0b5cdb10a1b4fb81ec3339b3171138857fa2
mlevenson88 <mlevenson@rocketmail.com>
2017-11-16 10:19:24 -0600

    Fixed nav bar bugs

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,19 @@
 /* select the class attached to the button */
 const navToggle = document.querySelector('.menu-button');
+const nav = document.querySelector('nav');
+const containerAll = document.querySelector('.container-all');
+
+/* shorten the var names */
+const containerAllStyle = containerAll.style;
+const bodyClassList = document.body.classList;
 
 /* add class .nav-is-open to the body when button is clicked */
 navToggle.addEventListener('click', function(){
-    document.body.classList.toggle('nav-is-open');
+    containerAllStyle.transition = 'transform 250ms ease-in-out';
+    bodyClassLis.toggle('nav-is-open');
+})
+
+nav.addEventListener('click', function(){
+    containerAllStyle.transition = '0ms';
+    bodyClassLis.remove('nav-is-open')
 })
